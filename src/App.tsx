4b0e9aa05787c46ccebc4516d6c8e6b39f7186d0
vinicius.santos <vinicius.santos@ifood.com.br>
2024-12-02 17:39:03 -0300

    Resolved problem to deploy on vercell

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import {
   Routes,
   Route,
   useLocation,
-  Navigate,
 } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./Main.css";
 import Header from "../../components/Header/Header";
